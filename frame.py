6396af204d8223d14986332a5d0444c9d01cbcd5
LaserWave2017 <laserwave@institutoptique.fr>
2017-07-26 15:27:29 +0100

    Update frame.py

--- frame.py
+++ frame.py
@@ -9,33 +9,40 @@ Created on 11 nov. 2014
 import pygame
 
 class Frame(object):
-	'''
-	classdocs
-	'''
-
-
 	def __init__(self):
-		'''
-		Constructor
-		'''
 		self.point_list = []
 
 	def LineTo(self, xy, c):
-		self.point_list.append((xy + (c,)))#tuple(c)
+		'''
+		USAGE   : - Frame.LineTo(tuple,int) 
+		RESUME  : - Crée un tuple entre une liste de coordonées spatiale et un entier colorimétrique.
+		ENTREE  : - xy : liste (x,y) d'un point.
+				: - c  : entier hexadécimale codant la couleur.
+		SORTIES : - VOID
+		'''
+		self.point_list.append((xy + (c,))) # Ecrire (c,) permet de le convertir en mono-tuple. 
 	
 	def Line(self, xy1, xy2, c):
 		self.LineTo(xy1,0)
 		self.LineTo(xy2,c)
 	
 	def PolyLineOneColor(self, xy_list, c, closed=False):
-		# code compatible avec les générateurs
-		xy0 = None
-		for xy in xy_list:
-			if xy0 is None:
-				xy0 = xy
-				self.LineTo(xy0,0)
-			else:
-				self.LineTo(xy,c)
+		'''
+		USAGE   : - Frame.PolyLineOneColor(list of tuple, int, boolean)
+		RESUME  : - Crée une liste de 2 tuples contenant la position x,y et la couleur de deux points formant une ligne,
+				    le premier point à la couleur blanche, le dernier à la couleur donnée par c.
+		ENTREES : - xy_list est la liste des coordonnées spatiales sous forme de tuples.
+				  - c  : entier hexadécimale codant la couleur.
+				  - closed : Définit la fin de la ligne si égal à True. False sinon.
+		SORTIES : - VOID
+		'''
+		xy0 = None   # Si aucun point n'a été utilisé pour le moment.
+		for xy in xy_list:  # Alors pour chaque points dans la liste.
+			if xy0 is None: # Si aucun point n'a encore été analysé.
+				xy0 = xy    # Alors enregistrer le premier point.
+				self.LineTo(xy0,0) # Lui assigner la couleur blanche.
+			else:                  # 
+				self.LineTo(xy,c) 
 		if closed:
 			self.LineTo(xy0,c)
 	
